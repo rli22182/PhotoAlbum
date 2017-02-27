ac33f4bd38633c0e99ef2c7a9d3d4b4cd8868e88
Ali Kian <akian@pss-mbp-12305.attlocal.net>
2017-02-26 21:52:06 -0800

    some clean up

--- src/main/java/com/alikian/controller/SwaggerConfig.java
+++ src/main/java/com/alikian/controller/SwaggerConfig.java
@@ -1,5 +1,6 @@
 package com.alikian.controller;
 
+import com.google.common.base.Predicates;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.PathSelectors;
@@ -19,7 +20,7 @@ public class SwaggerConfig {
         return new Docket(DocumentationType.SWAGGER_2)
                 .select()
                 .apis(RequestHandlerSelectors.any())
-                .paths(PathSelectors.any())
+                .paths(Predicates.not(PathSelectors.regex("/error.*")))
                 .build();
     }
 }
