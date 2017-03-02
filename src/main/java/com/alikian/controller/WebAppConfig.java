c3bce055e21bedb485bb268c1b0f87d9b70d378a
alikian <alikian@gmail.com>
2017-03-01 23:40:52 -0800

    Fixed routing issue

---
+++ src/main/java/com/alikian/controller/WebAppConfig.java
@@ -0,0 +1,25 @@
+package com.alikian.controller;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * Created by ali on 3/1/17.
+ */
+@Configuration
+public class WebAppConfig {
+
+    @Bean
+    public WebMvcConfigurerAdapter forwardToIndex() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/users/**").setViewName(
+                        "forward:/index.html");
+            }
+        };
+    }
+
+}
